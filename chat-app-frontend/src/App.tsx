b1756b607e88d85c1f3f9c6cd9dcb9cf5588ed49
brefosco <brefosco@hotmail.com>
2023-05-16 10:09:52 -0300

    WIP - chat working with author, msg & timestp TODO: * Show connected users * Show message with author * 0 styling

--- chat-app-frontend/src/App.tsx
+++ chat-app-frontend/src/App.tsx
@@ -1,22 +1,32 @@
-import React, { useState, useEffect } from 'react';
-import { io, Socket } from 'socket.io-client';
+import React, { useState, useEffect } from "react";
+import { io, Socket } from "socket.io-client";
 
-const socket: Socket = io('http://localhost:3000');
+const socket: Socket = io("http://localhost:3000");
 
-function MessagesList(messages: any) {
-  console.log(messages)
-
-  // const { messages } = messages
-  return (
-    <ul>
-      {messages?.messages?.map((msg, index) => (
-        <li key={index}>{msg}</li>
-      ))}
-    </ul>
-  )
+interface Message {
+  author: string;
+  text: string;
+  timestamp: number;
 }
 
-function UsernameForm({ username, handleUsernameChange, handleUsernameSubmit }: any) {
+function MessagesList({ messages }: { messages: Message[] }) {
+  return (
+    <ul style={{ listStyle: "none" }}>
+      {messages.map((msg, index) => (
+        <li key={index}>
+          <strong>{msg.author}</strong>: {msg.text}{" "}
+          {/* <em>{new Date(msg.timestamp).toLocaleString()}</em> */}
+        </li>
+      ))}
+    </ul>
+  );
+}
+
+function UsernameForm({
+  username,
+  handleUsernameChange,
+  handleUsernameSubmit,
+}: any) {
   return (
     <form onSubmit={handleUsernameSubmit}>
       <input
@@ -27,11 +37,14 @@ function UsernameForm({ username, handleUsernameChange, handleUsernameSubmit }: 
       />
       <button type="submit">Set Username</button>
     </form>
-  )
+  );
 }
 
-
-function MessageForm({ message, handleMessageSubmit, handleMessageChange }: any) {
+function MessageForm({
+  message,
+  handleMessageSubmit,
+  handleMessageChange,
+}: any) {
   return (
     <form onSubmit={handleMessageSubmit}>
       <input
@@ -42,38 +55,47 @@ function MessageForm({ message, handleMessageSubmit, handleMessageChange }: any)
       />
       <button type="submit">Send</button>
     </form>
-  )
+  );
+}
+
+function UsersList({ users }: { users: Record<string, string> }) {
+  return (
+    <div>
+      <h3>Connected users: </h3>
+
+      <ul style={{ listStyle: "none" }}>
+        {Object.values(users)?.map((user, index) => (
+          <li key={index}>{user}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 function App() {
-  const [username, setUsername] = useState('');
+  const [username, setUsername] = useState("");
   const [users, setUsers] = useState<Record<string, string>>({});
-  const [message, setMessage] = useState('');
-  const [messages, setMessages] = useState<string[]>([]);
+  const [message, setMessage] = useState("");
+  const [messages, setMessages] = useState<Message[]>([]);
 
-  const [isUsernameSelected, setIsUsernameSelected] = useState<boolean>(false)
+  const [isUsernameSelected, setIsUsernameSelected] = useState<boolean>(false);
 
+  const handleNewMessage = (msg: Message) => {
+    setMessages((messages) => [...messages, msg]);
+  };
 
   useEffect(() => {
-    socket.on('connect', () => {
-      console.log('Connected to server');
-      // Now you can start emitting events
-    });
-
-    socket.on('users', (users: Record<string, string>) => {
+    socket.on("users", (users: Record<string, string>) => {
       setUsers(users);
     });
 
-    socket.on('chat-message', (msg: string) => {
-      console.log('socket msg')
-      console.log(msg)
-      setMessages((messages) => [...messages, msg]);
-    });
+    socket.on("chat message", handleNewMessage);
 
     return () => {
-      socket.disconnect();
+      if (socket.connected) socket.disconnect();
+      socket.off("chat message", handleNewMessage);
     };
-  }, [setMessages]);
+  }, []);
 
   const handleUsernameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setUsername(e.target.value);
@@ -81,59 +103,43 @@ function App() {
 
   const handleUsernameSubmit = (e: React.FormEvent) => {
     e.preventDefault();
-    setIsUsernameSelected(true)
-    socket.emit('set_username', username);
+    setIsUsernameSelected(true);
+    socket.emit("set_username", username);
   };
 
   const handleMessageChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    // e.preventDefault()
     setMessage(e.target.value);
   };
 
   const handleMessageSubmit = (e: React.FormEvent) => {
     e.preventDefault();
-    console.log('handling submit')
-    socket.emit('chat-message', message);
-    setMessage('');
+    socket.emit("chat message", message);
+    setMessage("");
   };
-
-
-
-
-  function UsersList(users: any) {
-    console.log('users')
-    console.log(users)
-
-    return (
-      <ul>
-        {Object.values(users)?.map((user) => (
-          <li key={user}>{user}</li>
-        ))}
-      </ul>)
-  }
-  console.log(messages)
 
   return (
     <div>
-      <UsernameForm username={username} handleUsernameChange={handleUsernameChange} handleUsernameSubmit={handleUsernameSubmit} />
-      {isUsernameSelected ? <div>
-        Hello {username}
-        <MessageForm message={message} handleMessageChange={handleMessageChange} handleMessageSubmit={handleMessageSubmit} />
-        {/* <MessagesList messages={messages} /> */}
-
-        {/* <UsersList users={users} /> */}
-      </div>
-        : <p>Please select a username</p>}
-      {/* <ul>
-        {Object.values(users).map((user) => (
-          <li key={user}>{user}</li>
-        ))}
-      </ul>
-      <ul>
-        {messages.map((msg, index) => (
-          <li key={index}>{msg}</li>
-        ))}
-      </ul> */}
+      {isUsernameSelected ? (
+        <div>
+          Hello {username}
+          <MessageForm
+            message={message}
+            handleMessageChange={handleMessageChange}
+            handleMessageSubmit={handleMessageSubmit}
+          />
+          <MessagesList messages={messages} />
+          <UsersList users={users} />
+        </div>
+      ) : (
+        <div>
+          <p>Please select a username</p>
+          <UsernameForm
+            username={username}
+            handleUsernameChange={handleUsernameChange}
+            handleUsernameSubmit={handleUsernameSubmit}
+          />
+        </div>
+      )}
     </div>
   );
 }
--- index.ts
+++ index.ts
@@ -1,7 +1,7 @@
-import express from 'express';
-import { createServer } from 'http';
-import { Server } from 'socket.io';
-import cors from "cors"
+import express from "express";
+import { createServer } from "http";
+import { Server } from "socket.io";
+import cors from "cors";
 // ...
 const app = express();
 app.use(cors()); // use cors middleware
@@ -9,37 +9,58 @@ app.use(cors()); // use cors middleware
 const httpServer = createServer(app);
 
 const io = new Server(httpServer, {
-    cors: {
-        origin: "*",
-        methods: ["GET", "POST"],
-        allowedHeaders: ["my-custom-header"],
-        credentials: true
-    }
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+    // allowedHeaders: ["my-custom-header"],
+    credentials: true,
+  },
 });
 
 const users: Record<string, string> = {}; // {socketId: username}
+let userSockets: Record<string, string> = {}; // Key is username, value is socket id
 
-io.on('connection', (socket) => {
-    console.log('a user connected');
+io.on("connection", (socket) => {
+  console.log("a user connected");
 
-    // When a user sets their username
-    socket.on('set_username', (username: string) => {
-        users[socket.id] = username;
-        io.emit('users', users); // Broadcast the updated user list
+  // When a user sets their username
+  socket.on("set_username", (username: string) => {
+    users[socket.id] = username;
+    userSockets[username] = socket.id;
+    io.emit("users", users); // Broadcast the updated user list
+  });
+
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+    delete users[socket.id]; // Remove the disconnected user
+    io.emit("users", users); // Broadcast the updated user list
+  });
+
+  socket.on("chat message", (msg: string) => {
+    const username = users[socket.id];
+    io.emit("chat message", {
+      author: username,
+      text: msg,
+      timestamp: Date.now(),
     });
+  });
 
-    socket.on('disconnect', () => {
-        console.log('user disconnected');
-        delete users[socket.id]; // Remove the disconnected user
-        io.emit('users', users); // Broadcast the updated user list
-    });
-
-    socket.on('chat-message', (msg: string) => {
-        console.log('message: ' + msg);
-        io.emit('chat-message', msg);
-    });
+  //   socket.on("private message", ({ recipient, text }, callback) => {
+  //     const sender = users[socket.id];
+  //     const recipientSocketId = userSockets[recipient];
+  //     if (recipientSocketId) {
+  //       io.to(recipientSocketId).emit("private message", {
+  //         author: sender,
+  //         text,
+  //         timestamp: Date.now(),
+  //       });
+  //       callback();
+  //     } else {
+  //       callback("Recipient not found");
+  //     }
+  //   });
 });
 
 httpServer.listen(3000, () => {
-    console.log('listening on *:3000');
+  console.log("listening on *:3000");
 });
